4d31d8f68fc90c0d9ff7b15b687042e3f3f81419
Akanlegum John <akanlegumjohn2@gmail.com>
2023-01-12 21:11:11 +0000

    Added  a page that contains the details of a single character

---
+++ src/components/Character.jsx
@@ -0,0 +1,73 @@
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+
+const Character = () => {
+  const { charactersDetails } = useSelector((state) => state.characters);
+
+  const { characterId } = useParams();
+  const character = charactersDetails?.results.find(
+    (char) => char.id === Number(characterId)
+  );
+  console.log(character);
+  let createdDate = new Date(character.created);
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+  return (
+    <>
+      <section className="single--character--container">
+        <div className="img--container">
+          <img src={character.image} alt={character.name} />
+        </div>
+        <p>
+          This character goes by the name
+          <span className="character--name">{character.name}.</span>
+          {character.gender === 'Male' ? 'He' : 'She'} is a
+          <span>
+            {character.species}, {character.gender}
+          </span>
+          and is currently
+          {character.status === 'Alive' && (
+            <span style={{ color: 'green', fontWeight: '700' }}>
+              {character.status}.
+            </span>
+          )}
+          {character.status === 'unknown' && (
+            <span style={{ color: 'black', fontWeight: '700' }}>
+              {character.status}.
+            </span>
+          )}
+          {character.status === 'Dead' && (
+            <span style={{ color: 'red', fontWeight: '700' }}>
+              {character.status}.
+            </span>
+          )}
+          {character.gender === 'Male' ? 'He' : 'She'} is from
+          {character.origin.name === 'unknown' ? (
+            <span>an unknown origin </span>
+          ) : (
+            <span>{character.origin.name}</span>
+          )}
+          and is currently located at the{' '}
+          <span className="char--location">{character.location.name}.</span>
+          <span className="char--name">{character.name}</span> was created on
+          <span className="date">
+            {createdDate.toLocaleString('en-US', options)}.
+          </span>
+          <div className="back--link--container">
+            <Link to="/" className="back--to--char--link">
+              <span>Back To Characters</span>
+            </Link>
+          </div>
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Character;
