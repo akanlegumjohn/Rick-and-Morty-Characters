2ce0ab2282e2e643066f7792a5970e1dabff22a8
Akanlegum John <akanlegumjohn2@gmail.com>
2023-01-18 08:48:36 +0000

    Added debouncing to enhance performance in the searching of characters

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,21 @@
 // import { BsSearch } from 'react-icons/bs';
 import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
 
 import { setSearchedName } from '../features/characters/characterSlice';
 
 const Searchbar = () => {
   const dispatch = useDispatch();
-  const handleOnchange = (e) => {
-    dispatch(setSearchedName(e.target.value));
-  };
+  // const handleOnchange = (e) => {
+  //   dispatch(setSearchedName(e.target.value));
+  // };
+  const debouncedSearch = _.debounce((searchValue) => {
+    dispatch(setSearchedName(searchValue));
+  }, 500);
 
+  function handleOnchange(event) {
+    debouncedSearch(event.target.value);
+  }
   return (
     <form className="search">
       <input
@@ -16,10 +23,6 @@ const Searchbar = () => {
         type="text"
         placeholder="Search for characters..."
       />
-      {/* <button className=" search--btn" onClick={(e) => e.preventDefault()}>
-        <BsSearch />
-        <span>Search</span>
-      </button> */}
     </form>
   );
 };
